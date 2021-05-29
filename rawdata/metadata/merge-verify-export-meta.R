a46f32a086a6e31ff6606d7840bad5406f384238
Kevin Murray <foss@kdmurray.id.au>
2021-05-29 14:07:10 +1000

    more meta

---
+++ rawdata/metadata/merge-verify-export-meta.R
@@ -0,0 +1,18 @@
+# # Omni-ath metadata import, validation, and merge
+#
+# 
+
+library(tidyverse)
+#install.packages(c("tidyverse", "sp", "sf", "ggplot2", "ggmap", "validate"))
+
+#
+
+kg_sra = read_csv("source/1kg/1kg-sra-run-table.csv")
+
+kg_acc = read_csv("source/1kg/1kg-accessions.csv")
+
+african_acc = read_csv("source/africans/tabula-pnas.1616736114.sapp.csv", na='-')
+
+
+
+
